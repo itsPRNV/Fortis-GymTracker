327d2e47af11f94bd2bd5884bdd1029037ac972c
itsPRNV <kpranavr@gmail.com>
2025-08-01 19:43:14 +0530

    feat: new font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'providers/theme_provider.dart';
 import 'providers/workout_provider.dart';
 import 'providers/user_provider.dart';
@@ -31,11 +32,31 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.blue,
               brightness: Brightness.light,
               useMaterial3: true,
+              textTheme: GoogleFonts.montserratTextTheme(
+                Theme.of(context).textTheme,
+              ).copyWith(
+                headlineLarge: GoogleFonts.montserrat(fontWeight: FontWeight.bold),
+                headlineMedium: GoogleFonts.montserrat(fontWeight: FontWeight.bold),
+                headlineSmall: GoogleFonts.montserrat(fontWeight: FontWeight.bold),
+                titleLarge: GoogleFonts.montserrat(fontWeight: FontWeight.bold),
+                titleMedium: GoogleFonts.montserrat(fontWeight: FontWeight.w600),
+                titleSmall: GoogleFonts.montserrat(fontWeight: FontWeight.w600),
+              ),
             ),
             darkTheme: ThemeData(
               primarySwatch: Colors.blue,
               brightness: Brightness.dark,
               useMaterial3: true,
+              textTheme: GoogleFonts.montserratTextTheme(
+                ThemeData.dark().textTheme,
+              ).copyWith(
+                headlineLarge: GoogleFonts.montserrat(fontWeight: FontWeight.bold, color: Colors.white),
+                headlineMedium: GoogleFonts.montserrat(fontWeight: FontWeight.bold, color: Colors.white),
+                headlineSmall: GoogleFonts.montserrat(fontWeight: FontWeight.bold, color: Colors.white),
+                titleLarge: GoogleFonts.montserrat(fontWeight: FontWeight.bold, color: Colors.white),
+                titleMedium: GoogleFonts.montserrat(fontWeight: FontWeight.w600, color: Colors.white),
+                titleSmall: GoogleFonts.montserrat(fontWeight: FontWeight.w600, color: Colors.white),
+              ),
             ),
             themeMode: themeProvider.themeMode,
             home: const HomeScreen(),
